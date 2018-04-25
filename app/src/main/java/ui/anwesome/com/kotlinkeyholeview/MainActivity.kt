6f15cd40356454bc87fbc17ebdb9b14175add5d6
Anwesh43 <anweshthecool0@gmail.com>
2018-04-25 08:21:18 +0530

    adding KeyHoleView in activity

--- app/src/main/java/ui/anwesome/com/kotlinkeyholeview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinkeyholeview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinkeyholeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.keyholeview.KeyHoleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        KeyHoleView.create(this)
     }
 }
